26e07bba07d47bc4a47df8e56b91f6f1bd4382d2
Landry Kapela <landrykapela@gmail.com>
2020-08-24 09:09:42 +0300

    Add landing page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./images/siar_logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="App-logo" alt="Siar logo" />
+        <p></p>
+        <p>Siar Project by Landry Kapela (Team-1101)</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://buildforsdg.andela.com/program"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          #BuildForSDGCohort2
         </a>
       </header>
     </div>
